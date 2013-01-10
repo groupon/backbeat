229bda3ff081c17465d55fbefae2657a6b034466
Naren Chainani <naren@groupon.com>
2013-01-10 15:57:56 -0600

    Set up the environment stuff

--- server.rb
+++ server.rb
@@ -4,6 +4,8 @@ require 'bundler/setup'
 $: << File.expand_path(File.join(__FILE__, "..", "lib"))
 
 require 'goliath'
+ENV['RACK_ENV'] = Goliath.env.to_s
+
 require 'api'
 require 'awesome_print'
 require 'mongoid'
@@ -12,10 +14,9 @@ require 'delayed_job_mongoid'
 require 'tree'
 require 'mongoid_indifferent_access'
 
-#require 'workflow_server'
-
 class Server < ::Goliath::API
   use Api::Authenticate
+  use Goliath::Rack::Params
 
   def response(env)
     Api::Workflow.call(env)
