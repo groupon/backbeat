9868211ad0d39e1c118f8ef4b838ffb0d76cd62a
Naren Chainani <naren@groupon.com>
2013-01-14 23:26:09 -0600

    Fix activity integration specs

--- spec/integration/api/activity_spec.rb
+++ spec/integration/api/activity_spec.rb
@@ -36,61 +36,54 @@ describe Api::Workflow do
           json_response = JSON.parse(last_response.body)
           json_response['error'].should == "Activity #{activity.name} can't transition from open to completed"
         end
+
         it "returns 400 if the next decision is invalid" do
-          with_api(Server) do |api|
-            activity = FactoryGirl.create(:activity, status: :executing, parent: FactoryGirl.create(:decision))
-            wf = activity.workflow
-            user = wf.user
-            put_request(path: "/workflows/#{wf.id}/events/#{activity.id}/change_status", head: {"CLIENT_ID" => user.client_id}, query: {status: :completed, args: {next_decision: :test_decision}.to_json}) do |c|
-              c.response_header.status.should == 400
-              activity.reload
-              json_response = JSON.parse(c.response)
-              json_response['error'].should == "activity:#{activity.name} tried to make test_decision the next decision but is not allowed to."
-              activity.status.should_not == :complete
-            end
-          end
+          decision = FactoryGirl.create(:decision)
+          activity = FactoryGirl.create(:activity, status: :executing, parent: decision, workflow: decision.workflow)
+          wf = activity.workflow
+          user = wf.user
+          put "/workflows/#{wf.id}/events/#{activity.id}/change_status", {status: :completed, args: {next_decision: :test_decision}.to_json}
+          last_response.status.should == 400
+          activity.reload
+          json_response = JSON.parse(last_response.body)
+          json_response['error'].should == "activity:#{activity.name} tried to make test_decision the next decision but is not allowed to."
+          activity.status.should_not == :complete
         end
+
         it "returns 200 if the next decision is valid and the activity succeeds" do
-          with_api(Server) do |api|
-            activity = FactoryGirl.create(:activity, status: :executing, parent: FactoryGirl.create(:decision))
-            wf = activity.workflow
-            user = wf.user
-            put_request(path: "/workflows/#{wf.id}/events/#{activity.id}/change_status", head: {"CLIENT_ID" => user.client_id}, query: {status: :completed}) do |c|
-              c.response_header.status.should == 200
-              activity.reload
-              activity.children.count.should == 1
-              child = activity.children.first
-              child.name.should == :make_initial_payment_succeeded
-              activity.status.should == :complete
-            end
-          end
+          decision = FactoryGirl.create(:decision)
+          activity = FactoryGirl.create(:activity, status: :executing, parent: decision, workflow: decision.workflow)
+          wf = activity.workflow
+          user = wf.user
+          put "/workflows/#{wf.id}/events/#{activity.id}/change_status", {status: :completed}
+          last_response.status.should == 200
+          activity.reload
+          activity.children.count.should == 1
+          child = activity.children.first
+          child.name.should == :make_initial_payment_succeeded
+          activity.status.should == :complete
         end
       end
       context "activity errored" do
         it "returns 400 if the activity is not in executing state" do
-          with_api(Server) do |api|
-            activity = FactoryGirl.create(:activity, status: :open)
-            wf = activity.workflow
-            user = wf.user
-            put_request(path: "/workflows/#{wf.id}/events/#{activity.id}/change_status", head: {"CLIENT_ID" => user.client_id}, query: {status: :errored}) do |c|
-              c.response_header.status.should == 400
-              json_response = JSON.parse(c.response)
-              json_response['error'].should == "Activity #{activity.name} can't transition from open to errored"
-            end
-          end
+          activity = FactoryGirl.create(:activity, status: :open)
+          wf = activity.workflow
+          user = wf.user
+          put "/workflows/#{wf.id}/events/#{activity.id}/change_status", {status: :errored}
+          last_response.status.should == 400
+          json_response = JSON.parse(last_response.body)
+          json_response['error'].should == "Activity #{activity.name} can't transition from open to errored"
         end
+
         it "returns 200 and records the error message" do
-          with_api(Server) do |api|
-            activity = FactoryGirl.create(:activity, status: :executing, retry: 0)
-            wf = activity.workflow
-            user = wf.user
-            put_request(path: "/workflows/#{wf.id}/events/#{activity.id}/change_status", head: {"CLIENT_ID" => user.client_id}, query: {status: :errored, args: {error: {a: 1, b: 2}}.to_json}) do |c|
-              c.response_header.status.should == 200
-              activity.reload
-              activity.status.should == :error
-              activity.status_history.last["error"].should == {"a"=>1, "b"=>2}
-            end
-          end
+          activity = FactoryGirl.create(:activity, status: :executing, retry: 0)
+          wf = activity.workflow
+          user = wf.user
+          put "/workflows/#{wf.id}/events/#{activity.id}/change_status", {status: :errored, args: {error: {a: 1, b: 2}}.to_json}
+          last_response.status.should == 200
+          activity.reload
+          activity.status.should == :error
+          activity.status_history.last["error"].should == {"a"=>1, "b"=>2}
         end
       end
     end
@@ -98,41 +91,35 @@ describe Api::Workflow do
 
   context "PUT /workflows/:id/events/:event_id/run_sub_activity" do
     it "raises 404 when run_sub_activity is called on something other than activity" do
-      with_api(Server) do |api|
-        signal = FactoryGirl.create(:signal, status: :open)
-        wf = signal.workflow
-        user = wf.user
-        put_request(path: "/workflows/#{wf.id}/events/#{signal.id}/run_sub_activity", head: {"CLIENT_ID" => user.client_id}) do |c|
-          c.response_header.status.should == 404
-          json_response = JSON.parse(c.response)
-          json_response['error'].should == "Event with id(#{signal.id}) not found"
-        end
-      end
+      signal = FactoryGirl.create(:signal, status: :open)
+      wf = signal.workflow
+      user = wf.user
+      sub_activity = { name: :make_initial_payment, actor_type: "LineItem", actor_id: 100, retry: 100, retry_interval: 5, arguments: [1,2,3]}
+      put "/workflows/#{wf.id}/events/#{signal.id}/run_sub_activity", {sub_activity: sub_activity.to_json}
+      last_response.status.should == 404
+      json_response = JSON.parse(last_response.body)
+      json_response['error'].should == "Event with id(#{signal.id}) not found"
     end
+
     it "returns 400 when run_sub_activity is called while activity is not in executing state" do
-      with_api(Server) do |api|
-        activity = FactoryGirl.create(:activity, status: :open)
-        wf = activity.workflow
-        user = wf.user
-        put_request(path: "/workflows/#{wf.id}/events/#{activity.id}/run_sub_activity", head: {"CLIENT_ID" => user.client_id}) do |c|
-          c.response_header.status.should == 400
-          json_response = JSON.parse(c.response)
-          json_response['error'].should == "Cannot run subactivity while in status(#{activity.status})"
-        end
-      end
+      activity = FactoryGirl.create(:activity, status: :open)
+      wf = activity.workflow
+      user = wf.user
+      sub_activity = { name: :make_initial_payment, actor_type: "LineItem", actor_id: 100, retry: 100, retry_interval: 5, arguments: [1,2,3]}
+      put "/workflows/#{wf.id}/events/#{activity.id}/run_sub_activity", {sub_activity: sub_activity.to_json}
+      last_response.status.should == 400
+      json_response = JSON.parse(last_response.body)
+      json_response['error'].should == "Cannot run subactivity while in status(#{activity.status})"
     end
     it "runs the sub-activity" do
-      with_api(Server) do |api|
-        signal = FactoryGirl.create(:activity, status: :executing)
-        wf = signal.workflow
-        user = wf.user
-        sub_activity = { name: :make_initial_payment, actor_type: "LineItem", actor_id: 100, retry: 100, retry_interval: 5, arguments: [1,2,3]}
-        put_request(path: "/workflows/#{wf.id}/events/#{signal.id}/run_sub_activity", head: {"CLIENT_ID" => user.client_id}, query: {sub_activity: sub_activity.to_json}) do |c|
-          c.response_header.status.should == 200
-          json_response = JSON.parse(c.response)
-          c.response_header["WAIT_FOR_SUB_ACTIVITY"].should == "true"
-        end
-      end
+      signal = FactoryGirl.create(:activity, status: :executing)
+      wf = signal.workflow
+      user = wf.user
+      sub_activity = { name: :make_initial_payment, actor_type: "LineItem", actor_id: 100, retry: 100, retry_interval: 5, arguments: [1,2,3]}
+      put "/workflows/#{wf.id}/events/#{signal.id}/run_sub_activity", {sub_activity: sub_activity.to_json}
+      last_response.status.should == 200
+      json_response = JSON.parse(last_response.body)
+      last_response["WAIT_FOR_SUB_ACTIVITY"].should == "true"
     end
 
     it "returns 400 if some of the required parameters are missing" do
