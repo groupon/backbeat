9fc702b81c6196aacbe057aa84a3bff366deafb7
Naren Chainani <naren@groupon.com>
2013-01-16 15:09:32 -0600

    some improvements in the middleware

--- lib/api/camel_case.rb
+++ lib/api/camel_case.rb
@@ -11,11 +11,21 @@ module Api
           new_body = response.body.map {|str| JSON.parse(str) }
           ::Api::HashKeyTransformations.camelize_keys(new_body)
           response.body = new_body.map(&:to_json)
-          headers['Content-Length'] = response.body.inject(0) {|size, str| size += str.size }.to_s
+          headers['Content-Length'] = content_length(response.body, headers)
         rescue Exception => e
         end
       end
       [ status, headers, response ]
+    end
+
+    private
+
+    def content_length(body, headers)
+      body.inject(0) {|size, str| size += Rack::Utils.bytesize(str) }.to_s
+    end
+
+    def chunked?(headers)
+      "chunked" == headers['Transfer-Encoding']
     end
   end
 end
