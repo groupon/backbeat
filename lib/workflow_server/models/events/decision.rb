09354ef18c8be0cb60142927026939cbabf25748
Naren Chainani <naren@groupon.com>
2013-01-16 17:44:39 -0600

    .to_s before comparing the decision type

--- lib/workflow_server/models/events/decision.rb
+++ lib/workflow_server/models/events/decision.rb
@@ -125,7 +125,7 @@ module WorkflowServer
       end
 
       def add_decision(options = {})
-        case options.delete(:type)
+        case options.delete(:type).to_s
         when 'flag'
           add_flag(options[:name])
         when 'timer'
