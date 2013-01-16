f649a4d38b4dd6f60d49e398e88caa4af9bdca9e
Naren Chainani <naren@groupon.com>
2013-01-16 15:31:33 -0600

    unit specs for the camel case middleware

---
+++ spec/unit/api/camel_case_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Api::CamelCase do
+  before do
+    @env = {}
+    @body = {'actor_type' => "1", "actor_id" => 2, "deep_nest" => {"nested_inside" => "dont_touch_values"}}.to_json
+    @updated_body = {"actorType"=>"1", "actorId"=>2, "deepNest"=>{"nestedInside"=>"dont_touch_values"}}.to_json
+    @mock_app = lambda { |env|
+      @env.merge!(env)
+      Rack::Response.new(@body, 200, {'Content-Type' => 'application/json'})
+    }
+  end
+
+  it "converts response keys to camelCases" do
+    request = Rack::MockRequest.new(Rack::Lint.new(Api::CamelCase.new(@mock_app)))
+    response = request.get("http://someplace", {params: {abc: 20, bcd_abc: 500}})
+    json_response = JSON.parse(response.body)
+    json_response.should == JSON.parse(@updated_body)
+  end
+
+  it "doesn't change the response if content type is not application/json" do
+    @mock_app = lambda { |env|
+      @env.merge!(env)
+      Rack::Response.new(@body, 200, {'Content-Type' => 'application/xml'})
+    }
+    request = Rack::MockRequest.new(Rack::Lint.new(Api::CamelCase.new(@mock_app)))
+    response = request.get("http://someplace", {params: {abc: 20, bcd_abc: 500}})
+    json_response = JSON.parse(response.body)
+    json_response.should == {"actor_type"=>"1", "actor_id"=>2, "deep_nest"=>{"nested_inside"=>"dont_touch_values"}}
+  end
+
+  it "updates the content length header" do
+    request = Rack::MockRequest.new(Rack::Lint.new(Api::CamelCase.new(@mock_app)))
+    response = request.get("http://someplace", {params: {abc: 20, bcd_abc: 500}})
+    response.headers['Content-Length'].to_i.should_not == @body.size
+    response.headers['Content-Length'].to_i.should == @updated_body.size
+  end
+end
