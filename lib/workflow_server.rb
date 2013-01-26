3c3734334ee253089fd6121bd03306b36b9cf46b
Naren Chainani <naren@groupon.com>
2013-01-26 16:47:51 -0600

    Hooks to track delayed job log messages

--- lib/workflow_server.rb
+++ lib/workflow_server.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 
 module WorkflowServer
-  require 'workflow_server/logger'
   require 'workflow_server/config'
+  require 'workflow_server/logger'
   require 'workflow_server/helper'
   require 'workflow_server/errors'
   require 'workflow_server/models'
