69daea2aa9d56394f65c04fe6f00c8166f11e1cc
Jason Yanowitz <jyanowitz@groupon.com>
2013-01-30 16:44:10 -0600

    fixed init script typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,5 @@
-role :utility, "accounting-utility1-uat.snc1", :primary => true
-role :delayed_job, "accounting-utility1-uat.snc1"
-role :db, "accounting-mongodb1.snc1", :primary => true
-role :db, "accounting-mongodb2.snc1"
-role :db, "accounting-mongodb3.snc1"
+role :utility, "accounting-utility2.snc1", :primary => true
+role :delayed_job, "accounting-utility2.snc1"
 
 set :branch, ENV["branch"] if ENV["branch"]
 set :branch, ENV["BRANCH"] if ENV["BRANCH"]
