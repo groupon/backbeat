09f0b5126dc1ea29bdf1627f9ae27f5b7154841d
Naren Chainani <naren@groupon.com>
2013-01-31 11:52:57 -0600

    Response from health endpoint should be an array

--- lib/api/health.rb
+++ lib/api/health.rb
@@ -7,7 +7,7 @@ module Api
 
     def call(env)
       if env['PATH_INFO'].match /\/health/i
-        return [ 200, {"Content-Type" => "text/plain"}, WorkflowServer::Models::Workflow.last.try(:created_at).to_s ]
+        return [ 200, {"Content-Type" => "text/plain"}, [WorkflowServer::Models::Workflow.last.try(:created_at).to_s] ]
       end
       status, headers, body = @app.call(env)
       [status, headers, body]
