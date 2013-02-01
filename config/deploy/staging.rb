2f1398070186cb3026cc908173bfc1271cea5a6b
Matt Moore <mmoore@groupon.com>
2013-02-01 16:02:28 -0600

    update uat and staging deploys

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-role :utility, "accounting-utility1-staging.snc1", :primary => true
-role :delayed_job, "accounting-utility1-staging.snc1"
+role :utility, "fed1-tat.snc1", :primary => true
+role :delayed_job, "fed1-tat.snc1"
 set :branch, ENV["branch"] if ENV["branch"]
 set :branch, ENV["BRANCH"] if ENV["BRANCH"]
--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -1,4 +1,4 @@
-role :utility, "accounting-utility1-uat.snc1", :primary => true
-role :delayed_job, "accounting-utility1-uat.snc1"
+role :utility, "fed2-tat.snc1", :primary => true
+role :delayed_job, "fed2-tat.snc1"
 set :branch, ENV["branch"] if ENV["branch"]
 set :branch, ENV["BRANCH"] if ENV["BRANCH"]
