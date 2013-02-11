caffd10248201e0d892563a1bfdbcda68228968b
Jason Yanowitz <jyanowitz@groupon.com>
2013-02-11 12:18:46 -0600

    updated backbeat for new UAT/Staging hosts. paired with naren

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-role :utility, "fed1-tat.snc1", :primary => true
-role :delayed_job, "fed1-tat.snc1"
+role :utility, "accounting-utility2-staging.snc1", :primary => true
+role :delayed_job, "accounting-utility2-staging.snc1"
 set :branch, ENV["branch"] if ENV["branch"]
 set :branch, ENV["BRANCH"] if ENV["BRANCH"]
--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -1,4 +1,4 @@
-role :utility, "fed2-tat.snc1", :primary => true
-role :delayed_job, "fed2-tat.snc1"
+role :utility, "accounting-utility2-uat.snc1", :primary => true
+role :delayed_job, "accounting-utility2-uat.snc1"
 set :branch, ENV["branch"] if ENV["branch"]
 set :branch, ENV["BRANCH"] if ENV["BRANCH"]
