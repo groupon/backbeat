aafd589cbe8f6d27280a73673ef3d9783eaec99f
Matt Moore <mmoore@groupon.com>
2013-02-14 10:21:32 -0600

    noop to test any_branch endpoint

--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -1,4 +1,5 @@
 role :utility, "accounting-utility2-uat.snc1", :primary => true
 role :delayed_job, "accounting-utility2-uat.snc1"
+
 set :branch, ENV["branch"] if ENV["branch"]
 set :branch, ENV["BRANCH"] if ENV["BRANCH"]
