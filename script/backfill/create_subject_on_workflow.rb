3374c4445e46e0619faa76a268d11d27df6e9548
Matt Moore <mmoore@groupon.com>
2013-02-25 17:36:27 -0600

    fix backfill script

--- script/backfill/create_subject_on_workflow.rb
+++ script/backfill/create_subject_on_workflow.rb
@@ -1,6 +1,6 @@
 #assumes subject is not on the document
 WorkflowServer::Models::Workflow.field :subject, type: Hash, default: {}
 WorkflowServer::Models::Workflow.each do |wf|
-  wf.subject = { subject_id: wf.subject_id.to_s, subject_klass: wf.subject_klass }
+  wf.subject = { subject_id: wf.subject_id, subject_klass: wf.subject_klass }
   wf.save!
 end
