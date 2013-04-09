215ec5dd6e2a7e5b17ec711f7dac9a60e7675f5d
Naren Chainani <naren@groupon.com>
2013-04-09 11:19:19 -0500

    Part 1 - Fix the set of failed delayed jobs.
    Delayed jobs that fail try to call the 'error' hook on the payload job object. Including WorkflowServer::Logger added a error method to the job. Delayed job believed this error method was the error hook and tried to call it when the job failed.

--- spec/unit/workflow_server/async/job_spec.rb
+++ spec/unit/workflow_server/async/job_spec.rb
@@ -11,13 +11,15 @@ describe WorkflowServer::Async::Job do
   end
   context "#perform" do
     before do
+      @logger = mock('logger', info: nil, error: nil)
+      WorkflowServer::Logger.stub(logger: @logger)
       @job = WorkflowServer::Async::Job.schedule({event: decision, method: :some_method, args: [1,2,3,4], max_attempts: 100}, Time.now + 2.days)
       @dec = mock('decision', some_method: nil, id: 10, name: :make_payment)
       WorkflowServer::Models::Event.stub(find: @dec)
     end
     it "logs start and succeeded messages" do
-      @job.payload_object.should_receive(:info).with(id: 10, name: :make_payment, message: "some_method_started")
-      @job.payload_object.should_receive(:info).with(id: 10, name: :make_payment, message: "some_method_succeeded")
+      @logger.should_receive(:info).with(source: "WorkflowServer::Async::Job", id: 10, name: :make_payment, message: "some_method_started")
+      @logger.should_receive(:info).with(source: "WorkflowServer::Async::Job", id: 10, name: :make_payment, message: "some_method_succeeded")
       @job.invoke_job
     end
     it "calls the method on the given event" do
@@ -28,7 +30,7 @@ describe WorkflowServer::Async::Job do
     context '#error' do
       it 'records exceptions and raises the error' do
         @dec.should_receive(:some_method).and_raise('some error')
-        @job.payload_object.should_receive(:error).with(id: 10, name: :make_payment, message: "some_method_errored", error: anything, backtrace: anything)
+        @logger.should_receive(:error).with(source: "WorkflowServer::Async::Job", id: 10, name: :make_payment, message: "some_method_errored", error: anything, backtrace: anything)
         expect {
           @job.invoke_job
         }.to raise_error
