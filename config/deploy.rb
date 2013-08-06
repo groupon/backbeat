f756641021e965bfe28aebcbc45c6241de13c95e
Jason Yanowitz <jyanowitz@groupon.com>
2013-08-06 15:10:39 -0500

    actually wire up resque rake tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -195,7 +195,7 @@ end
 namespace :workers do
   [:start, :stop, :restart, :status].each do |command|
     desc "#{command} worker processes on utility box"
-    task command, :roles => [:delayed_job] do
+    task command, :roles => [:delayed_job, :resque_backbeat_server] do
       worker_init_scripts.each do |script|
         run "/usr/local/etc/init.d/#{script}_#{application} #{command}"
       end
