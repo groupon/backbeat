fd703b025e1066ca3166457702383930cdd4c3f0
Naren Chainani <naren@groupon.com>
2013-08-21 12:21:58 -0500

    get the specs going

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'http://rubygems.org'
-ruby '1.9.2'
 
 gem 'rake'
 
--- app.rb
+++ app.rb
@@ -28,3 +28,32 @@ require 'newrelic_rpm'
 
 
 
+############################################## MONKEY-PATCH ################################################
+## FIX JRUBY TIME MARSHALLING - SEE https://github.com/rails/rails/issues/10900 ############################
+require 'active_support/core_ext/time/marshal' # add crappy marshalling first and then overwrite it
+
+class Time
+  class << self
+    def _load(marshaled_time)
+      time = _load_without_zone(marshaled_time)
+      time.instance_eval do
+        if isdst_and_zone = defined?(@_isdst_and_zone) && remove_instance_variable('@_isdst_and_zone')
+          ary = to_a
+          ary[0] += subsec if ary[0] == sec
+          ary[-2, 2] = isdst_and_zone
+          utc? ? Time.utc(*ary) : Time.local(*ary)
+        else
+          self
+        end
+      end
+    end
+  end
+
+  def _dump(*args)
+    obj = dup
+    obj.instance_variable_set('@_isdst_and_zone', [dst?, zone])
+    obj.send :_dump_without_zone, *args
+  end
+end
+
+############################################### MONKEY-PATCH OVER ############################################
