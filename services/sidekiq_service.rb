8bbded6928e3ab9ccc283a5a46ace5cc4bc88205
Naren Chainani <naren@groupon.com>
2013-09-27 11:03:34 -0500

    fix sidekiq service options parser

--- services/sidekiq_service.rb
+++ services/sidekiq_service.rb
@@ -15,11 +15,11 @@ module Services
   class SidekiqService
     attr_accessor :config, :launcher, :start_failed
 
-    CONFIG_OPTIONS_TO_STRIP = [:config_file, :daemon, :environment, :pidfile, :require, :tag, :options]
+    CONFIG_OPTIONS_TO_STRIP = ['config_file', 'daemon', 'environment', 'pidfile', 'require', 'tag', 'options']
 
     def initialize(opts = {})
       # We merge options here because some keys will cause an error to be raised in torquebox.rb when using the configuration DSL
-      @config = opts.symbolize_keys.reject { |k, _| CONFIG_OPTIONS_TO_STRIP.include?(k) }.merge(opts[:options])
+      @config = opts.reject { |k, _| CONFIG_OPTIONS_TO_STRIP.include?(k) }.merge(opts['options']).symbolize_keys
       @mutex = Mutex.new
 
       redis_config = YAML::load_file("./config/redis.yml")[WorkflowServer::Config.environment.to_s]
@@ -44,7 +44,6 @@ module Services
 
     def run
       Sidekiq.options.merge!(config)
-      Sidekiq.options[:environment] = WorkflowServer::Config.environment
       raise 'Sidekiq workers must have at least 1 queue!' if Sidekiq.options[:queues].size < 1
 
       Sidekiq::Logging.logger = WorkflowServer::SidekiqLogger
