e7d4b76e80b3cb7f6c2510327ca27c08ea9345de
Matt Moore <mmoore@groupon.com>
2013-11-01 08:58:47 -0500

    fix spec for sidekiq latency endpoint

--- spec/integration/api/sidekiq_latency_spec.rb
+++ spec/integration/api/sidekiq_latency_spec.rb
@@ -7,11 +7,12 @@ describe Api::SidekiqLatency do
     FullRackApp
   end
 
-  context "/sidekiq_latency" do
-    it "catches the request, returns 200 and the queue latency" do
+  context '/sidekiq_latency' do
+    it 'catches the request, returns 200 and the queue latency' do
+      Sidekiq::Queue.stub_chain(:new, :latency).and_return(666)
       response = get '/sidekiq_latency'
       response.status.should == 200
-      response.body.should == "0"
+      response.body.should == '666'
     end
   end
 end
