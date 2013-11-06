9f016c9e2dca118b835256ef927437b668be1c67
Jason Yanowitz <jyanowitz@groupon.com>
2013-11-06 08:26:21 -0600

    updated sidekiq stats endpoint

---
+++ lib/api/sidekiq_stats.rb
@@ -0,0 +1,25 @@
+module Api
+  class SidekiqStats
+    def initialize(app)
+      @app = app
+    end
+
+    ENDPOINT = '/sidekiq_stats'
+    def call(env)
+      if env['PATH_INFO'] == ENDPOINT
+        stats = Sidekiq::Stats.new
+        data = {
+          latency: stats.queues.keys.inject({}) {|h,q| h[q] = Sidekiq::Queue.new(q).latency; h },
+          processed: stats.processed,
+          failed: stats.failed,
+          enqueued: stats.enqueued,
+          scheduled: stats.scheduled_size,
+          retry_size: stats.retry_size
+        }
+        return [ 200, {"Content-Type" => "application/json"}, [ data.to_json ] ]
+      end
+      status, headers, body = @app.call(env)
+      [status, headers, body]
+    end
+  end
+end
