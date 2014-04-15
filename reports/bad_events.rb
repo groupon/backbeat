74f2c389cbb8e24db8c1de6729aff24b7bbf2f72
Naren Chainani <naren@groupon.com>
2014-04-14 20:23:05 -0500

    Missed out one more change

--- reports/bad_events.rb
+++ reports/bad_events.rb
@@ -68,8 +68,7 @@ module Reports
       query.delete_if do |event|
         event.status == :error || event.status == :timeout ||
           (event.status == :resolved && event.parent.status == :complete) ||
-          (event.workflow && event.workflow.events.where(:status.in => [:error, :timeout]).exists?) ||
-          event.children.type(Timer).where(status: :scheduled, mode: :blocking, :fires_at.gt => Time.now).exists?
+          (event.workflow && event.workflow.events.where(:status.in => [:error, :timeout]).exists?)
       end
     end
 
