dc299849d5b324e27c67955fd2e6e19af74f55d2
Jason Yanowitz <jyanowitz@groupon.com>
2014-08-29 10:01:14 -0500

    fix test

--- spec/integration/api/health_spec.rb
+++ spec/integration/api/health_spec.rb
@@ -8,14 +8,15 @@ describe Api::Health do
   end
 
   context "/health" do
-    it "includes running SHA, current time and status" do
+    it "includes running SHA, current time and status. if this fails locally, run a mongo on port 27018" do
       response = get '/health'
+      ap  Mongoid.default_session.cluster.nodes.map(&:connected?)
       response.status.should == 200
       response.headers["Content-Type"].should == "application/json"
       JSON.parse(response.body).should == {
         "sha" => GIT_REVISION,
         "time" => Time.now.iso8601,
-        "status" => "DATABASE_UNREACHABLE" # it is in this test, but not under normal circumstances :)
+        "status" => "OK"
       }
     end
   end
