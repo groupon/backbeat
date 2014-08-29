68bdfe965fa0dd7fd1b66fd3c177fc367b6c2c5e
Jason Yanowitz <jyanowitz@groupon.com>
2014-08-29 12:58:54 -0500

    remove debugging info

--- spec/integration/api/health_spec.rb
+++ spec/integration/api/health_spec.rb
@@ -10,7 +10,6 @@ describe Api::Health do
   context "/health" do
     it "includes running SHA, current time and status. if this fails locally, run a mongo on port 27018" do
       response = get '/health'
-      ap  Mongoid.default_session.cluster.nodes.map(&:connected?)
       response.status.should == 200
       response.headers["Content-Type"].should == "application/json"
       JSON.parse(response.body).should == {
