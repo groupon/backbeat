dee6b4ace073f47ebabd4cf8be36ab658db7c9f0
shane <shane@groupon.com>
2014-12-11 11:59:08 -0600

    initial migrations and scripts to get started

--- script/migrate.rb
+++ script/migrate.rb
@@ -1,14 +1,8 @@
-dropdb backbeat && createdb backbeat 
-psql backbeat
-CREATE ROLE backbeat with LOGIN CREATEDB SUPERUSER;
-
-
-bin/console
 require 'active_record'
 require 'foreigner'
+require 'ap'
 config = YAML::load(IO.read('config/database.yml'))
 ActiveRecord::Base.establish_connection config['development']
 Foreigner.load
 ActiveRecord::Migrator.migrate('migrations', nil)
-
 
