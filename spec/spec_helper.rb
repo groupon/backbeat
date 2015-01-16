875d5f678def06b1d6fb949b51e3c7f181aee29a
Kevin Buchanan <kevaustinbuch@gmail.com>
2015-01-16 11:38:30 -0600

    refactors setup to speed up test start time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,41 +1,116 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter "/bin/"
-  add_filter "/config/"
-  add_filter "/spec/"
-end
-
-$: << File.expand_path(File.join(__FILE__, "../.."))
 $: << File.expand_path(File.join(__FILE__, ".."))
-
 ENV['RACK_ENV'] = "test"
 
 require 'bundler'
-require 'helper/mongo'
-
-Bundler.setup
-Bundler.require(:default, :test)
+Bundler.setup(:test)
 
 require 'app'
+require 'rspec'
+require 'rack/test'
+require 'factory_girl'
+require 'database_cleaner'
+require 'timecop'
+require 'webmock'
+require 'rspec-sidekiq'
 
-unless ENV["CONSOLE_LOG"] == "true"
+if ENV["SIMPLE_COV"]
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/bin/"
+    add_filter "/config/"
+    add_filter "/spec/"
+  end
+end
+
+unless ENV["CONSOLE_LOG"]
   log_dir = File.expand_path("../../log", __FILE__)
   Dir.mkdir(log_dir) unless File.exists?(log_dir)
   WorkflowServer::Logger.set_logger(Logger.new(File.open(File.expand_path("../../log/test.log", __FILE__), "a+")))
 end
 
-require_relative '../services/sidekiq_service'
-require_relative '../reports/daily_report'
-
-
-mongo_path = File.expand_path(File.join(__FILE__, "..", "..", "config", "mongoid.yml"))
-Mongoid.load!(mongo_path, :test)
-
-
 RACK_ROOT = File.expand_path(File.join(__FILE__,'..'))
 ENV['RACK_ROOT'] = RACK_ROOT
+FullRackApp = Rack::Builder.parse_file(File.expand_path(File.join(__FILE__,'..','..','config.ru'))).first
 
-require 'database_cleaner'
+FORMAT_TIME = Proc.new { |time| time.strftime("%Y-%m-%dT%H:%M:%SZ") }
+RSPEC_CONSTANT_USER_CLIENT_ID = UUIDTools::UUID.random_create.to_s
+BACKBEAT_CLIENT_ENDPOINT = "http://backbeat-client:9000"
+
+FactoryGirl.find_definitions
+
+def run_async_jobs
+  WorkflowServer::Async::Job.stub(:enqueue) do |job_data|
+    WorkflowServer::Workers::SidekiqJobWorker.new.perform(
+      job_data[:event].id,
+      job_data[:method],
+      job_data[:args],
+      job_data[:max_attempts]
+    )
+  end
+  yield
+  WorkflowServer::Async::Job.unstub(:enqueue)
+end
+
+RSpec::Sidekiq.configure do |config|
+  config.warn_when_jobs_not_processed_by_sidekiq = false
+end
+
+if Backbeat.v2?
+  RSpec.configure do |config|
+    config.filter_run_including v2: true
+
+    config.before(:each) do
+      Timecop.freeze(DateTime.now)
+    end
+
+    config.after(:each) do
+      Timecop.return
+    end
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    end
+
+    config.color_enabled = true
+    config.formatter = :documentation
+  end
+else
+  require 'helper/mongo'
+
+  RSpec.configure do |config|
+    config.filter_run_excluding v2: true
+
+    config.before(:suite) do
+      Helper::Mongo.start(27018)
+    end
+
+    config.after(:suite) do
+      Mongoid::Sessions.default.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+      Helper::Mongo.stop(27018)
+    end
+
+    config.before(:each) do
+      Timecop.freeze(DateTime.now)
+      response = double('response', code: 200)
+      WorkflowServer::Client.stub(post: response)
+    end
+
+    config.after(:each) do
+      Timecop.return
+      Mongoid::Sessions.default.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+    end
+
+    config.color_enabled = true
+    config.formatter = :documentation
+  end
+end
 
 ###### FOR TRANSACTION SUPPORT #########
 # We need this till we have tokumx 1.2.0 on build and dev machines
@@ -52,108 +127,3 @@ module WorkflowServer
   end
 end
 ###### FOR TRANSACTION SUPPORT END #####
-
-########### TORQUEBOX SPECIFIC STUFF - START #########################
-# delete any deployed yml files before starting
-FileUtils.rm_rf("#{WorkflowServer::Config.root}/.torquespec")
-
-# # torquebox has some internal calls to its management service running inside localhost. this
-# # conflicts with the webmock stubs
-WebMock.disable_net_connect!(:allow_localhost => true)
-
-BACKBEAT_APP = <<-DD_END.gsub(/^ {4}/,'')
-    application:
-        root: #{WorkflowServer::Config.root}
-    environment:
-        RACK_ENV: test
-    DD_END
-
-################ TORQUEBOX SPECIFIC STUFF - END #########################
-
-########### MOCK BACKBEAT CLIENT START #################
-BACKBEAT_CLIENT_ENDPOINT = "http://backbeat-client:9000"
-service = nil
-if defined?(JRUBY_VERSION) && FakeTorquebox.run_jboss?
-  require_relative 'service/backbeat_client'
-  service = Service::BackbeatClient.new('backbeat-test')
-  BACKBEAT_CLIENT_ENDPOINT = service.start(3010)
-else
-  def deploy(*args)
-    # no-op on MRI
-  end
-
-  def remote_describe(*args)
-    describe *args do
-      yield
-    end
-  end
-end
-########### MOCK BACKBEAT CLIENT END   #################
-
-FORMAT_TIME = Proc.new { |time| time.strftime("%Y-%m-%dT%H:%M:%SZ") }
-
-FullRackApp = Rack::Builder.parse_file(File.expand_path(File.join(__FILE__,'..','..','config.ru'))).first
-
-RSPEC_CONSTANT_USER_CLIENT_ID = UUIDTools::UUID.random_create.to_s
-
-FactoryGirl.find_definitions
-
-# should go in unit spec helper
-def run_async_jobs
-  WorkflowServer::Async::Job.stub(:enqueue) { |job_data| WorkflowServer::Workers::SidekiqJobWorker.new.perform(job_data[:event].id, job_data[:method], job_data[:args], job_data[:max_attempts]) }
-  yield
-  WorkflowServer::Async::Job.unstub(:enqueue)
-end
-
-RSpec.configuration.before(:each) do
-  Timecop.freeze(DateTime.now)
-  unless Backbeat.v2?
-    response = double('response', code: 200)
-    WorkflowServer::Client.stub(post: response)
-  end
-end
-
-RSpec.configuration.after(:each) do
-  Timecop.return
-  Mongoid::Sessions.default.collections.select {|c| c.name !~ /system/ }.each(&:drop)
-  service.try(:clear)
-end
-
-RSpec.configuration.before(:suite) do
-  Helper::Mongo.start(27018)
-end
-
-RSpec.configuration.after(:suite) do
-  Mongoid::Sessions.default.collections.select {|c| c.name !~ /system/ }.each(&:drop)
-  Helper::Mongo.stop(27018)
-end
-
-RSpec.configure do |config|
-  if Backbeat.v2?
-    config.filter_run_including v2: true
-  else
-    config.filter_run_excluding v2: true
-  end
-
-  config.before(:suite) do
-    if Backbeat.v2?
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
-    end
-  end
-
-  if Backbeat.v2?
-    config.around(:each) do |example|
-      DatabaseCleaner.cleaning do
-        example.run
-      end
-    end
-  end
-
-
-  # Use color in STDOUT
-  config.color_enabled = true
-
-  # Use the specified formatter
-  config.formatter = :documentation # :progress, :html, :textmate
-end
