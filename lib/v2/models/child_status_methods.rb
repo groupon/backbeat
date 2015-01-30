dc9028fd39d1b92ce1b00065e4c5b595ad6d1a39
Jon Bauer <jbauer@groupon.com>
2015-01-30 14:13:23 -0600

    add print tree method

--- lib/v2/models/child_status_methods.rb
+++ lib/v2/models/child_status_methods.rb
@@ -1,5 +1,5 @@
 module V2
-  module ChildStatusMethods
+  module SharedNodeMethods
     def all_children_ready?
       !children.where(current_server_status: :pending).exists?
     end
@@ -10,6 +10,10 @@ module V2
 
     def all_children_complete?
       !not_complete_children.where("mode != 'fire_and_forget'").exists?
+    end
+
+    def print_tree
+      puts V2::WorkflowTree.to_string(self)
     end
   end
 end
