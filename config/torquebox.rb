758c83047f6b3bd4f7ee667403f345f3ee0f219c
Jon Bauer <jbauer@groupon.com>
2015-04-14 17:24:45 -0500

    change cron comments, change syntax

--- config/torquebox.rb
+++ config/torquebox.rb
@@ -41,6 +41,9 @@ TorqueBox.configure do
     end
   end
 
+
+  # For cron syntax see - http://torquebox.org/documentation/2.3.0/scheduled-jobs.html
+
   if `hostname` =~ /accounting-utility2/
     job Reports::LogQueueCounts do
       # Every 5 minutes
@@ -51,13 +54,13 @@ TorqueBox.configure do
   if ENV['RACK_ENV'] == 'production' &&
     `hostname` =~ /accounting-utility2/
     job Reports::InconsistentNodes do
-      # Every day at midnight
-      cron '0 0 12 1/1 * ? *'
+      # Every day at noon
+      cron '0 0 12 * * ?'
     end
 
     job Reports::DailyReport do
-      # Every day at midnight
-      cron '0 0 12 1/1 * ? *'
+      # Every day at noon
+      cron '0 0 12 * * ?'
     end
 
     job Reports::BadEvents do
