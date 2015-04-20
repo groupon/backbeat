cf7afe61988dfb6a1ccdf651d73077e46e0a161a
Jon Bauer <jbauer@groupon.com>
2015-04-19 22:21:04 -0500

    add more route details to logging, add spec

--- lib/api/middleware/log.rb
+++ lib/api/middleware/log.rb
@@ -20,11 +20,20 @@ module Api
                           type: envs[1],
                           method: envs.last,
                           env: env['PATH_INFO'],
-                          duration: Time.now - t0})
+                          duration: Time.now - t0,
+                          route_info: route_info(env)})
 
         headers[TRANSACTION_ID_HEADER] = tid
         WorkflowServer::Logger.tid(:clear)
         [status, headers, body]
+      end
+
+      def route_info(env)
+        route_info = env["rack.routing_args"].try(:[], :route_info)
+        if route_info
+          options = route_info.instance_variable_get("@options")
+          options.slice(:version, :namespace, :method, :path)
+        end
       end
     end
   end
