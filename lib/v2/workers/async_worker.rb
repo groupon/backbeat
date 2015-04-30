c307e607c3ab777967b928852bee9dbf711d220c
Kevin Buchanan <c_kbuchanan@groupon.com>
2015-04-30 10:30:23 -0500

    Refactor async worker retries

--- lib/v2/workers/async_worker.rb
+++ lib/v2/workers/async_worker.rb
@@ -20,32 +20,61 @@ module V2
 
       def perform(event_class, node_data, options)
         info(status: :perform_started, node_data: node_data, event: event_class, options: options)
+        business_perform(event_class, node_data, options)
+        info(status: :perform_finished, node: node_data, event: event_class)
+      end
+
+      private
+
+      def business_perform(event_class, node_data, options)
         options = symbolize_keys(options)
-        node = deserialize_node(event_class, node_data, options)
-        Server.fire_event(event_class.constantize, node, Async::PerformEvent)
-        info(status: :perform_finished, node: node.id, event: event_class)
+        process_event(event_class, node_data, options)
+      rescue NodeServerError=> e
+        handle_processing_error(e, event_class, node_data, options) do |e|
+          Server.fire_event(Events::ServerError, e.node)
+        end
       rescue => e
+        handle_processing_error(e, event_class, node_data, options) do |e|
+          info(status: :deserialize_node_error, error: e, backtrace: e.backtrace)
+        end
+      end
+
+      def handle_processing_error(e, event_class, node_data, options)
         retries = options.fetch(:retries, 4)
         if retries > 0
           new_options = options.merge(retries: retries - 1)
           AsyncWorker.perform_at(Time.now + 30.seconds, event_class, node_data, new_options)
         else
-          if node
-            Server.fire_event(Events::ServerError, node)
-          else
-            info(status: :deserialize_node_error, error: e, backtrace: e.backtrace)
-          end
+          yield e
         end
         raise e
       end
 
-      private
+      def process_event(event_class, node_data, options)
+        node = deserialize_node(node_data)
+        fire(event_class, node)
+      end
+
+      class NodeServerError < StandardError
+        attr_reader :node
+
+        def initialize(node, e)
+          @node = node
+          super(e.message)
+        end
+      end
+
+      def fire(event_class, node)
+        Server.fire_event(event_class.constantize, node, Async::PerformEvent)
+      rescue => e
+        raise NodeServerError.new(node, e)
+      end
 
       def symbolize_keys(options)
         options.reduce({}) { |m, (k, v)| m[k.to_sym] = v; m }
       end
 
-      def deserialize_node(event_class, node_data, options)
+      def deserialize_node(node_data)
         node_class = node_data["node_class"]
         node_id = node_data["node_id"]
         node_class.constantize.find(node_id)
