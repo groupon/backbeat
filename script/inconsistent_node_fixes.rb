1fe4f55067483f4ceadafe77cdf18a22161f1002
Kevin Buchanan <c_kbuchanan@groupon.com>
2015-07-20 10:43:02 -0500

    Remove v2 references

--- script/inconsistent_node_fixes.rb
+++ script/inconsistent_node_fixes.rb
@@ -5,31 +5,31 @@
 time = Time.now - 20.hours
 
 #For checking inconsistent nodes
-V2::Node.where("fires_at < ?", time).where("(current_server_status <> 'complete' OR current_client_status <> 'complete') AND current_server_status <> 'deactivated'").count
+Backbeat::Node.where("fires_at < ?", time).where("(current_server_status <> 'complete' OR current_client_status <> 'complete') AND current_server_status <> 'deactivated'").count
 
-nodes = V2::Node.where(current_server_status: :processing_children, current_client_status: :complete).where("fires_at < ?", time);1
+nodes = Backbeat::Node.where(current_server_status: :processing_children, current_client_status: :complete).where("fires_at < ?", time);1
 nodes.each do |n|
-  V2::Events::ScheduleNextNode.call(n)
+  Backbeat::Events::ScheduleNextNode.call(n)
 end
 
-nodes = V2::Node.where("fires_at < ?", time).where(current_server_status: :started, current_client_status: :ready);1
+nodes = Backbeat::Node.where("fires_at < ?", time).where(current_server_status: :started, current_client_status: :ready);1
 nodes.each do |n|
-  V2::Events::StartNode.call(n)
+  Backbeat::Events::StartNode.call(n)
 end
 
-nodes = V2::Node.where("fires_at < ?", time).where(current_server_status: :sent_to_client, current_client_status: :received);1
+nodes = Backbeat::Node.where("fires_at < ?", time).where(current_server_status: :sent_to_client, current_client_status: :received);1
 nodes.each do |n|
-  V2::Events::ScheduleNextNode.call(n.parent)
+  Backbeat::Events::ScheduleNextNode.call(n.parent)
 end
 
-nodes = V2::Node.where(current_server_status: :sent_to_client, current_client_status: :received).where("fires_at < ?", time);1
+nodes = Backbeat::Node.where(current_server_status: :sent_to_client, current_client_status: :received).where("fires_at < ?", time);1
 nodes.each do |n|
   if n.children.count == 0
-    V2::Client.perform_action(n)
+    Backbeat::Client.perform_action(n)
   end
 end
 
-nodes = V2::Node.where("fires_at < ?", time).where(current_server_status: :ready, current_client_status: :ready);1
+nodes = Backbeat::Node.where("fires_at < ?", time).where(current_server_status: :ready, current_client_status: :ready);1
 nodes.each do |n|
-  V2::Events::ScheduleNextNode.call(n.parent)
+  Backbeat::Events::ScheduleNextNode.call(n.parent)
 end
