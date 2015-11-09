4ba8962aa6b38a3c53b8d3231d171f788bb52b4d
Kevin Buchanan <c_kbuchanan@groupon.com>
2015-11-09 17:08:30 -0600

    Tweak daily activity report

--- lib/backbeat/workers/daily_activity.rb
+++ lib/backbeat/workers/daily_activity.rb
@@ -43,24 +43,26 @@ module Backbeat
 
       def perform
         start_time = Time.now
+        report_range = report_range(start_time)
 
+        inconsistent_nodes = inconsistent_nodes(report_range)
+        inconsistent_counts = counts_by_workflow_type(inconsistent_nodes)
         inconsistent_ids = ids_grouped_by_workflow(inconsistent_nodes)
         file_name = write_to_file(inconsistent_ids)
 
-        inconsistent_counts = counts_by_workflow_type(inconsistent_nodes)
+        completed_nodes = completed_nodes(report_range)
         completed_counts = counts_by_workflow_type(completed_nodes)
 
         report_data = {
           inconsistent: {
             counts: inconsistent_counts,
-            options: inconsistent_node_options,
             filename: file_name
           },
           completed: {
             counts: completed_counts,
-            options: completed_node_options
           },
-          time_elapsed: (Time.now - start_time).to_i
+          time_elapsed: (Time.now - start_time).to_i,
+          range: report_range
         }
 
         send_report(report_data)
@@ -68,40 +70,57 @@ module Backbeat
 
       private
 
-      def inconsistent_node_options
+      def report_range(start)
         {
-          lower_bound: Time.parse('2015/04/01'),
-          upper_bound: Date.today
+          lower_bound: start - 24.hours,
+          upper_bound: start
         }
       end
 
-      def completed_node_options
-        upper_bound = Time.now
-        {
-          lower_bound: upper_bound - 24.hours,
-          upper_bound: upper_bound
-        }
-      end
-
-      def inconsistent_nodes
+      def inconsistent_nodes(range)
         Backbeat::Node
-          .where("fires_at > ?", inconsistent_node_options[:lower_bound])
-          .where("fires_at < ?", inconsistent_node_options[:upper_bound])
+          .where("fires_at > ?", range[:lower_bound])
+          .where("fires_at < ?", range[:upper_bound])
           .where("(current_server_status <> 'complete' OR current_client_status <> 'complete') AND current_server_status <> 'deactivated'")
       end
 
-      def completed_nodes
+      def completed_nodes(range)
         Backbeat::Node
-          .where("fires_at > ?", completed_node_options[:lower_bound])
-          .where("fires_at < ?", completed_node_options[:upper_bound])
+          .where("fires_at > ?", range[:lower_bound])
+          .where("fires_at < ?", range[:upper_bound])
           .where("current_server_status = 'complete' AND current_client_status = 'complete'")
       end
 
       def write_to_file(nodes_by_workflow)
-        file_name = "/tmp/inconsistent_nodes/#{Date.today}.txt"
+        file_name = "/tmp/inconsistent_nodes/#{Date.today}.json"
         FileUtils.mkdir_p(File.dirname(file_name))
         File.open(file_name, 'w') { |file| file.write(nodes_by_workflow.to_json) }
         file_name
+      end
+
+      def counts_by_workflow_type(nodes_arel)
+        nodes_arel
+          .joins(:workflow)
+          .reorder("")
+          .group("workflows.name")
+          .select("workflows.name, count(distinct workflow_id) as workflow_type_count, count(*) as node_count")
+          .reduce({}) do |results, result|
+            results[result.name] = {
+              workflow_type_count: result.workflow_type_count,
+              node_count: result.node_count
+            }
+            results
+          end
+      end
+
+      def ids_grouped_by_workflow(nodes_arel)
+        nodes_arel
+          .select("id, workflow_id")
+          .reorder("")
+          .reduce(Hash.new { |h, k| h[k] = [] }) do |results, result|
+            results[result.workflow_id] << result.id
+            results
+          end
       end
 
       def send_report(report)
@@ -115,31 +134,6 @@ module Backbeat
             body Report.render(report)
           end
         end
-      end
-
-      def counts_by_workflow_type(nodes_arel)
-        counts_by_type = {}
-        results = nodes_arel.joins(:workflow)
-          .reorder("")
-          .group("workflows.name")
-          .select("workflows.name, count(distinct workflow_id) as workflow_type_count, count(*) as node_count")
-          .all.each do |result|
-          counts_by_type[result.name] = {
-            workflow_type_count: result.workflow_type_count,
-            node_count: result.node_count
-          }
-        end
-        counts_by_type
-      end
-
-      def ids_grouped_by_workflow(nodes_arel)
-        nodes_by_workflow = {}
-        nodes_arel.select("id,workflow_id")
-          .reorder("")
-          .group_by(&:workflow_id).each_pair do |workflow_id,nodes|
-          nodes_by_workflow[workflow_id] = nodes.map(&:id)
-        end
-        nodes_by_workflow
       end
 
       class Report
@@ -159,7 +153,7 @@ module Backbeat
           ).result(binding)
         end
 
-        def render_table(title, counts, options)
+        def render_table(title, counts)
           ERB.new(
             File.read(File.expand_path('../daily_activity/table.html.erb', __FILE__))
           ).result(binding)
