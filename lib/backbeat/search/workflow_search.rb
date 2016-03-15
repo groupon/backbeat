0ed6c9f2ae9c3a750af06178c24474d0d911ae43
Courtney Brennaman <courtneyb@groupon.com>
2016-03-15 17:06:01 -0500

    create search/filter, refactor workflow and activity searches

--- lib/backbeat/search/workflow_search.rb
+++ lib/backbeat/search/workflow_search.rb
@@ -28,100 +28,45 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+require 'backbeat/search/filter'
+
 module Backbeat
   module Search
     class WorkflowSearch
-      def self.filter_for(param, default = nil, &block)
-        lambda do |relation, params|
-          value = params.fetch(param, default)
-          if value
-            block.call(relation, params.merge({ param => value }))
-          else
-            relation
-          end
-        end
-      end
-
       def initialize(params, user_id)
         @params = params
         @user_id = user_id
       end
 
       def result
-        apply_filters(
-          Workflow.where(user_id: user_id).order({ created_at: :desc, id: :desc }),
-          NameFilter,
-          SubjectFilter,
-          CurrentStatusFilter,
-          PastStatusFilter,
-          StatusStartFilter,
-          StatusEndFilter,
-          PerPageFilter,
-          PageFilter,
-          LastIdFilter
+        filter.apply_filters(
+          params,
+          filter.name,
+          subject_filter,
+          filter.current_status,
+          filter.past_status,
+          filter.status_start,
+          filter.status_end,
+          filter.per_page,
+          filter.page,
+          filter.last_id
         )
-      end
-
-      NameFilter = filter_for(:name) do |relation, params|
-        relation.where("workflows.name = ?", params[:name])
-      end
-
-      SubjectFilter = filter_for(:subject) do |relation, params|
-        relation.where("workflows.subject LIKE ?", "%#{params[:subject]}%")
-      end
-
-      CurrentStatusFilter = filter_for(:current_status) do |relation, params|
-        relation.joins(:nodes).where(
-          "nodes.current_server_status = ? OR nodes.current_client_status = ?",
-          params[:current_status],
-          params[:current_status]
-        )
-      end
-
-      PastStatusFilter = filter_for(:past_status) do |relation, params|
-        relation.joins(:nodes).joins("JOIN status_changes ON status_changes.node_id = nodes.id").where(
-          "status_changes.to_status = ?",
-          params[:past_status]
-        )
-      end
-
-      StatusStartFilter = filter_for(:status_start) do |relation, params|
-        relation.where("status_changes.created_at >= ?", params[:status_start])
-      end
-
-      StatusEndFilter = filter_for(:status_end) do |relation, params|
-        relation.where("status_changes.created_at <= ?", params[:status_end])
-      end
-
-      PAGE_SIZE = 25
-
-      PerPageFilter = filter_for(:per_page, PAGE_SIZE) do |relation, params|
-        limit = params[:per_page].to_i
-        relation.limit(limit)
-      end
-
-      PageFilter = filter_for(:page, 1) do |relation, params|
-        per_page = params.fetch(:per_page, PAGE_SIZE).to_i
-        page = params[:page].to_i
-        offset = (page - 1) * per_page
-        relation.offset(offset)
-      end
-
-      LastIdFilter = filter_for(:last_id) do |relation, params|
-        last_id = params.fetch(:last_id)
-        last_created_at = Workflow.where(id: last_id).pluck(:created_at).first
-        relation.where("(workflows.created_at, workflows.id) < (?, ?)", last_created_at, last_id)
       end
 
       private
 
       attr_reader :params, :user_id
 
-      def apply_filters(base, *filters)
-        return [] if params.empty?
-        filters.reduce(base) do |relation, filter|
-          filter.call(relation, params)
-        end.distinct
+      def filter
+        @filter ||= Filter.new(
+          Workflow.where(user_id: user_id).order({ created_at: :desc, id: :desc }).joins(:nodes)
+        )
+      end
+
+      def subject_filter
+        filter.filter_for(:subject) do |relation, params|
+          relation.where("workflows.subject LIKE ?", "%#{params[:subject]}%")
+        end
       end
     end
   end
